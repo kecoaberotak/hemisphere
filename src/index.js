1051489dba250a687ed831647231d57fb4c0b2fc
KecoaBerotak <dennisfernandes0212@gmail.com>
2022-01-28 17:00:40 +0700

    edit index.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render() {
+
+    constructor(props){
+        super(props)
+
+        this.state = {latitude: null, errorMessage: ''}
+
         // mengambil lokasi dari user
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (error) => console.log(error)
-        );
+            (position) => {
+                this.setState({latitude: position.coords.latitude})
+            },
+            (error) => {
+                this.setState({errorMessage: error.message})
+            }
+        );        
+    }
+    render() {
         return(
             <div>
-                You are in the Northen Hemisphere
+                {this.state.latitude}
+                {this.state.errorMessage}
             </div>
         )
     }
